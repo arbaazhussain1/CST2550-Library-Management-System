d37304c00b664263fe063b9b871dc045373924e1
Arbaaz <arbaazahussain786@gmail.com>
2024-01-22 13:23:14 +0000

    Adding Add appropriate comments to all headers files: Book.h, Librarian.h, Member.h & Person.h to the  Repository

--- Library Management System/Book.cpp
+++ Library Management System/Book.cpp
@@ -1,3 +1,4 @@
+// The Library Management System for the Book.cpp file.
 #include "Book.h"
 
 
@@ -11,53 +12,55 @@ Book::Book(int bookID, std::string bookName, std::string authorFirstName, std::s
     this->isIssued = isIssued;
 }
 
-// Accessor methods
+// Accessor methods: for Retrieving information.
 int Book::getBookID() const {
-    return bookID;
+    return bookID;  // Return the book's unique identifier.
 }
 
 std::string Book::getBookName() const {
-    return bookName;
+    return bookName;  // Return the name of the book.
 }
 
 std::string Book::getAuthorFirstName() const {
-    return authorFirstName;
+    return authorFirstName; // Return the author's first name.
 }
 
 std::string Book::getAuthorLastName() const {
-    return authorLastName;
-}
-void Book::setBookAsIssued(bool bookIssued) {
-    isIssued = bookIssued;
+    return authorLastName; // Return the author's last name.
 }
 
 std::string Book::getBookType() const {
-    return bookType;
+    return bookType;  // Return the type of the book.
 }
 
 std::time_t Book::getDueDate() const {
-    return dueDate;
+    return dueDate; // Return the due date for the book.
 }
 
-// Mutator methods
-void Book::setDueDate(std::time_t dueDate) {
-    this->dueDate = dueDate;
-}
 bool Book::isBookIssued() const {
-    return isIssued;
+    return isIssued;  // Check if the book is currently issued.
+}
+// Mutator methods: for to updating information.
+void Book::setDueDate(std::time_t dueDate) {
+    this->dueDate = dueDate; // Set a new due date for the book.
+}
+
+void Book::setBookAsIssued(bool bookIssued) {
+    isIssued = bookIssued; // Set the issuance status of the book.
 }
 
 void Book::returnBook() {
-    borrower = nullptr;
-    dueDate = 0;
+    borrower = nullptr; // Clear the borrower information.
+    dueDate = 0; // Reset the due date.
 }
 
 void Book::borrowBook(Member* borrower, std::time_t dueDate) {
-    this->borrower = borrower;
-    this->dueDate = dueDate;
+    this->borrower = borrower; // Set the borrower information.
+    this->dueDate = dueDate;  // Set the due date.
 }
-
+// Function to get a reference to the Book list in the Library Management System.
 std::vector<Book> &getBookList(){
+    // Static vector to persist the list of books across function calls.
     static std::vector<Book> bookList;
     return bookList;
 }
--- Library Management System/Member.cpp
+++ Library Management System/Member.cpp
@@ -1,8 +1,10 @@
 // The Library Management System for the Member.cpp file.
 
+// The derived class from Person, here the class will inherit/add the members of the Person class directly.
+
 #include "Member.h" // Include the header file of the Member class.
 
-// Constructor
+// The constructor of the Member object that is using in the Person class itself.
 Member::Member(int memberID, const std::string& name, const std::string& address, const std::string& email)
         : memberID(memberID), booksLoaned() {
     setName(name);
@@ -19,18 +21,18 @@ const std::vector<Book>& Member::getBooksBorrowed() const {
 }
 
 std::vector<Book>& Member::getBooksBorrowedWithSystem(){
-    return booksLoaned;
+    return booksLoaned; // Return a reference to the list of books borrowed by the member.
 }
 
 // Mutator methods: for to updating information.
 void Member::setMemberID(int newMemberID) {
-    memberID = newMemberID;
+    memberID = newMemberID; // Set a new member ID for the member.
 }
 
 void Member::setBooksBorrowed(const Book& book) {
-    booksLoaned.push_back(book);
+    booksLoaned.push_back(book); // Add a book to the list of books borrowed by the member.
 }
-
+// Function to get a reference to the Member list in the Library Management System.
 std::vector<Member> &Memberlist(){
     static std::vector<Member> memberList;
     return memberList;
